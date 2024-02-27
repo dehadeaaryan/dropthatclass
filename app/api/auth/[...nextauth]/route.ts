e91ed7e9f15e17b1b2493ba810a75443026ade01
dehadeaaryan <dehadeaaryan@gmail.com>
2024-02-26 22:45:00 -0600

    route.ts 7

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google";
-
-const authOptions: NextAuthOptions = {
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID ?? "",
-            clientSecret: process.env.GITHUB_SECRET ?? "",
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID ?? "",
-            clientSecret: process.env.GOOGLE_SECRET ?? "",
-        }),
-    ],
-}
+import NextAuth from 'next-auth';
+import authOptions from './options';
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST }
