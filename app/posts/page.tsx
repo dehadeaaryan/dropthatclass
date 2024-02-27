1538e594c6db461caca9ed648d8ec55006d2874e
dehadeaaryan <dehadeaaryan@gmail.com>
2024-02-27 11:11:51 -0600

    updated route.ts

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,8 +1,8 @@
 import { redirect } from "next/navigation";
-import { getServerSession } from "next-auth";
+import { auth } from "@/lib/auth";
 
 export default async function Page() {
-    const session = await getServerSession();
+    const session = await auth();
     if (!session || !session.user) {
         redirect("/api/auth/signin");
     }
