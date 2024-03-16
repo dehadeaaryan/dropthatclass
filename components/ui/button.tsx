9058816a92e79d8ae67d5a3d1163785847bb4aa3
dehadeaaryan <dehadeaaryan@gmail.com>
2024-03-16 16:59:38 -0500

    removed account button error

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-export default function Button({ children, props }: { children: React.ReactNode, props: any }) {
+export default function Button({ children, onClick=()=>{} }: { children: React.ReactNode, onClick?: () => any }) {
     return (
-        <button onClick={props ? props.func : ""} className="border hover:bg-white hover:text-black transition-all duration-200 text-white rounded p-2">
+        <button onClick={onClick} className="border hover:bg-white hover:text-black transition-all duration-200 text-white rounded p-2">
             {children}
         </button>
     );
