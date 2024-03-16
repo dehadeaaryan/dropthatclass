e54988b5d4dfc57babe2f6391f002d4b094b3c95
dehadeaaryan <dehadeaaryan@gmail.com>
2024-03-16 16:47:26 -0500

    build check

--- components/reviews.tsx
+++ components/reviews.tsx
@@ -2,7 +2,7 @@
 
 import { AddSvg } from "./svgs";
 
-export default async function Reviews(props: any) {
+export default function Reviews(props: any) {
     const reviews = props.reviews;
     return (
         <div className="flex flex-1 flex-col items-center justify-start px-8 py-4 gap-4">
@@ -15,7 +15,7 @@ export default async function Reviews(props: any) {
             <div id="reviews" className="flex flex-col items-center justify-start w-full"></div>
             {reviews.map((review: any) => {
                 return (
-                    <div className="flex flex-col w-full bg-neutral-900 rounded-2xl px-8 py-4">
+                    <div key={review._id} className="flex flex-col w-full bg-neutral-900 rounded-2xl px-8 py-4">
                         <div className="flex flex-row items-center justify-between w-full">
                             <p className="text-2xl font-bold">{review.author}</p>
                             <p className="text-lg">{review.professor}</p>
